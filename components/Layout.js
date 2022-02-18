4a193998291e439f7d480f447aa9b7d9d78d8e27
Azeezjim <azeexjim@gmail.com>
2022-02-18 14:35:22 -0800

    2nd  commit

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import { Box } from '@chakra-ui/react';
+
+const Layout = () => {
+    <>
+        <Head>
+            <Title>Azeezjim Houses & Suit</Title>
+        </Head>
+        <
+    </>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Router from 'next/router';
 import Head from 'next/head';
+import NProgress from 'nprogress';
 import {ChakaraProvider} from '@chakra-ui/react'
+
+import {Layout} from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
