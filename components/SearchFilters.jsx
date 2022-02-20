9a22fec3f7b92c6c63bedd119d9419ccc79a8959
Azeezjim <azeexjim@gmail.com>
2022-02-20 03:19:05 -0800

    3rd   commit

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import {Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md"
+import Image from "next/image";
+
+const SearchFIlter = () => {
+    return (
+        <div>
+            SearchFIlter
+        </div>
+    )
+}
+
+export default SearchFIlter;
--- pages/search.js
+++ pages/search.js
@@ -3,29 +3,36 @@ import { useRouter } from "next/router";
 import { Image } from "next/image";
 import { Flex, Box, Text, Icon } from '@chakra-ui/react'
 import { BsFilter  } from "react-icons/bs";
+import SearchFIlter from "../components/SearchFilters";
 
 const Search = () => {
     const [searchFilter, setSearchFliter ] = useState(false);
     const router = useRouter()
     
-    return (
+    return ( 
         <Box>
             <Flex 
                 cursor="pointer"
                 bg="gray.100"
+                borderBottom="1px"
+                borderColor="gray.200"
+                p="2"
+                fontWeight="black"
+                fontSize="lg"
+                justifyContent="center"
+                alignItems="center"
+                onClick={() => setSearchFliter((prevFilter) => !prevFilter)}
             >
-                <Text
-                    cursor="pointer"
-                    bg="gray.100"
-                    borderBottom="1px"
-                    p="2"
-                    fontWeight="black"
-                    fontSize="lg"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    Search Property By Filter</Text>
+            <Text>Search Property By Filter</Text>
+                <Icon pathLength="2" w="7" as={BsFilter} />
             </Flex>
+            {searchFilter && <SearchFIlter />}
+            <Text fontSize="2xl" p="4" fontWeight="bold">
+                properties
+                <Flex>
+
+                </Flex>
+            </Text>
         </Box>
     )
 }
